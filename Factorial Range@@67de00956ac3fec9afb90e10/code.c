7213a2d8ddbbb2a1485102e8d03357a3958c3b1c
Aryan-453 <aryandhiman453@gmail.com>
2025-03-27 19:05:31 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,21 @@
+#inlcude <stdio.h>
+void factorial(int n) {
+    if (n == 0 || n == 1) return 1;
+    int fact = 1;
+    for (int i = 2; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+void factorialrange(a,b){
+    int a,b;
+    for(i=a;i<=b;i++){
+        factorial(i)
+    }
+}
+int main(){
+int start,end;
+scanf("%d %d",&start, &end);
+factorialRange(start, end);
+return 0;
+}
